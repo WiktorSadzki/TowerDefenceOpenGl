13bfb699c3ad78866bb4de3610d520a104dfdc8d
sadzk <sadzkiwiktor@gmail.com>
2026-05-10 02:28:05 +0200

    troops

---
+++ TowerDefence/Road.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <vector>
+#include <glm/glm.hpp>
+#include <GL/glew.h>
+
+struct Vertex
+{
+    glm::vec3 Position;
+    glm::vec2 TexCoords;
+};
+
+class Road
+{
+public:
+
+    std::vector<glm::vec3> controlPoints;
+
+    std::vector<glm::vec3> splinePoints;
+
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+
+    GLuint VAO, VBO, EBO;
+
+    float roadWidth = 3.0f;
+
+    Road();
+
+    void GenerateSpline();
+    void GenerateMesh();
+    void SetupMesh();
+
+    void Draw();
+
+private:
+
+    glm::vec3 CatmullRom(
+        glm::vec3 p0,
+        glm::vec3 p1,
+        glm::vec3 p2,
+        glm::vec3 p3,
+        float t
+    );
+};
