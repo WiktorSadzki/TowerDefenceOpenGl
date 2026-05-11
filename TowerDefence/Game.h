26a8de11f3dfbc1d18d903dec883e951083cdfe4
sadzk <sadzkiwiktor@gmail.com>
2026-05-11 06:26:59 +0200

    animation troops hud

--- TowerDefence/Game.h
+++ TowerDefence/Game.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <GL/glew.h>
 #include "Camera.h"
+#include "Road.h"
+
+enum TileType { TILE_GRASS, TILE_PATH, TILE_TREE, TILE_MOUNTAIN };
+enum class TowerType { MACHINE_GUN = 0, ROCKETS = 1, SNIPER = 2 };
+enum TroopType { CAR, TANK, HELICOPTER };
+enum GameState { START_MENU, PLAYING, GAME_OVER };
 
 struct P {
     float x = 0.0f;
@@ -14,52 +21,95 @@ struct VertexData {
     float r, g, b;
 };
 
-enum class TowerType { MACHINE_GUN = 0, ROCKETS = 1, SNIPER = 2 };
-
 struct TowerGeometry {
     std::vector<VertexData> base_mesh;
     std::vector<VertexData> rotate_mesh;
     std::vector<VertexData> gun_mesh;
-
-    float base_y_offset;
-    float gun_y_offset;
-    float rotate_y_offset;
+    float base_y_offset = 0.0f;
+    float gun_y_offset = 0.0f;
+    float rotate_y_offset = 0.0f;
 };
 
 struct TowerInstance {
     float x = 0.0f;
     float z = 0.0f;
     float current_yaw = 0.0f;
+    float current_pitch = 0.0f;
     TowerType tower_variant = TowerType::MACHINE_GUN;
 };
 
 struct Troop {
-    float x, z;
-    int currentWaypoint;
-    float speed;
-    float health;
-    std::string type_name;
+    float x = 0.0f, z = 0.0f, altitude = 0.5f, rotation_yaw = 0.0f;
+    float health = 100.0f, speed = 8.0f;
+    int currentWaypoint = 0;
+    TroopType variant = CAR;
+};
+
+struct TroopGeometry {
+    std::vector<VertexData> mesh;
+};
+
+struct Projectile{
+    float x = 0.0f, y = 0.0f, z = 0.0f;
+    float vx = 0.0f, vy = 0.0f, vz = 0.0f;
+    float life_span = 0.0f;
 };
 
 class Game {
 public:
     int lives = 20;
     int gold = 100;
-    int wave = 1;
+    int wave = 0;
     bool gameOver = false;
+    bool waveActive = false;
+    int troopsRemainingInWave = 0;
+    float spawnTimer = 0.0f;
+
+    bool isBuilding = false;
+    TowerType ghostType = TowerType::MACHINE_GUN;
+
+    std::vector<Projectile> active_bullets;
+    float fire_cooldowns[1000] = { 0 };
+
+    void spawnProjectile(float sx, float sy, float sz, float tx, float ty, float tz);
+
+    void startNextWave();
+
+    bool raycastGroundPlane(float mx, float my, int winW, int winH, float& outX, float& outZ);
+
+    float ghost_wx = 0.0f;
+    float ghost_wz = 0.0f;
+
+    float waveEndMessageTimer = 0.0f;
+
+    static const int grid_dim = 24;
+    TileType map[grid_dim][grid_dim];
+    float height_map[grid_dim][grid_dim];
+
     std::vector<Troop> troops;
     std::vector<P> pathWaypoints;
-    void spawnTroop();
+    Road game_pathway;
 
     TowerType selectedType = TowerType::MACHINE_GUN;
     TowerGeometry tower_assets[3];
     std::vector<TowerInstance> active_defenses;
 
-    void loadTroopWave(std::string path);
+    TroopGeometry troop_assets[3];
+
     void init();
-    void update(float dt);
+    void update(float delta_step);
     void render();
-    void tryPlaceTower(int mx, int my, Camera& cam);
-    void loadModels(std::string path, std::string baseDir, std::vector<VertexData>& target);
-    void selectTowerType(int index);
+    void spawnTroop();
+
+    void loadMapFromImage(const std::string& texture_path);
+    void tileCenter(int col_idx, int row_idx, float& world_x_pos, float& world_z_pos);
+
+    void tryPlaceTower(int mouse_x, int mouse_y, Camera& world_camera);
+    void loadModels(std::string root_dir, std::string file_name, std::vector<VertexData>& vertex_buffer);
+    void loadTroopWave(std::string resource_path);
+    void selectTowerType(int list_index);
+    void toggleBuildMode(int typeIndex);
+
+private:
+    void renderHUD();
 };
--- TowerDefence/main.cpp
+++ TowerDefence/main.cpp
@@ -1,22 +1,22 @@
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <GL/glu.h>
+#include <GL/glut.h>
 #include <iostream>
+#include <cmath>
 #include "Game.h"
 #include "Camera.h"
 
 static Game   g_game;
 static Camera g_camera;
-static double g_lastMouseX = 0;
-static double g_lastMouseY = 0;
-static bool   g_mouseDown = false;
-static bool g_rightMouseDown = false;
-static bool g_mouseMoved = false;
-static float  g_lastTime = 0.0f;
-
-const float MAP_LIMIT = 45.0f;
-const float MOVE_SPEED = 30.0f;
+static double g_last_mouse_x = 0;
+static double g_last_mouse_y = 0;
+static bool   g_mouse_left_down = false;
+static bool   g_mouse_right_down = false;
+static bool   g_mouse_has_moved = false;
+static float  g_previous_time = 0.0f;
+const float   MAP_BOUNDARY = 45.0f;
+const float   CAMERA_MOVE_SPEED = 30.0f;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     if (height == 0) height = 1;
@@ -29,40 +29,53 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
     if (button == GLFW_MOUSE_BUTTON_LEFT) {
-        g_mouseDown = (action == GLFW_PRESS);
-        if (action == GLFW_PRESS) g_mouseMoved = false;
-        if (action == GLFW_RELEASE && !g_mouseMoved) {
+        g_mouse_left_down = (action == GLFW_PRESS);
+        if (action == GLFW_PRESS) {
+            g_mouse_has_moved = false;
+        }
+        if (action == GLFW_RELEASE && !g_mouse_has_moved) {
+            double current_x, current_y;
+            glfwGetCursorPos(window, &current_x, &current_y);
+            g_game.tryPlaceTower((int)current_x, (int)current_y, g_camera);
+        }
+    }
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE && !g_mouse_has_moved) {
+        if (g_game.isBuilding) {
             double x_coord, y_coord;
             glfwGetCursorPos(window, &x_coord, &y_coord);
             g_game.tryPlaceTower((int)x_coord, (int)y_coord, g_camera);
+            g_game.isBuilding = false;
         }
     }
     if (button == GLFW_MOUSE_BUTTON_RIGHT) {
-        g_rightMouseDown = (action == GLFW_PRESS);
+        g_mouse_right_down = (action == GLFW_PRESS);
     }
 }
 
 void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
-    float dx = (float)(xpos - g_lastMouseX);
-    float dy = (float)(ypos - g_lastMouseY);
+    float delta_x = (float)(xpos - g_last_mouse_x);
+    float delta_y = (float)(ypos - g_last_mouse_y);
 
-    if (g_mouseDown) {
-        g_mouseMoved = true;
-        float rad = g_camera.angleY * 3.14159f / 180.0f;
-        float fwdX = sinf(rad), fwdZ = -cosf(rad);
-        float rgtX = cosf(rad), rgtZ = sinf(rad);
-        float speed = g_camera.zoom * 0.002f;
-        g_camera.cx -= rgtX * dx * speed;
-        g_camera.cz -= rgtZ * dx * speed;
-        g_camera.cx += fwdX * dy * speed;
-        g_camera.cz += fwdZ * dy * speed;
+    if (g_mouse_left_down) {
+        g_mouse_has_moved = true;
+        float radian_yaw = g_camera.angleY * 3.14159f / 180.0f;
+        float forward_x = sinf(radian_yaw);
+        float forward_z = -cosf(radian_yaw);
+        float right_x = cosf(radian_yaw);
+        float right_z = sinf(radian_yaw);
+        float drag_sensitivity = g_camera.zoom * 0.002f;
+
+        g_camera.cx -= right_x * delta_x * drag_sensitivity;
+        g_camera.cz -= right_z * delta_x * drag_sensitivity;
+        g_camera.cx += forward_x * delta_y * drag_sensitivity;
+        g_camera.cz += forward_z * delta_y * drag_sensitivity;
     }
-    if (g_rightMouseDown) {
-        g_camera.handleMouse((int)dx, (int)dy);
+    if (g_mouse_right_down) {
+        g_camera.handleMouse((int)delta_x, (int)delta_y);
     }
 
-    g_lastMouseX = xpos;
-    g_lastMouseY = ypos;
+    g_last_mouse_x = xpos;
+    g_last_mouse_y = ypos;
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
@@ -73,35 +86,51 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (action == GLFW_PRESS) {
         if (key == GLFW_KEY_ESCAPE) glfwSetWindowShouldClose(window, true);
-        if (key == GLFW_KEY_R) g_game.init();
-
         if (key == GLFW_KEY_R) {
             g_game.init();
             g_camera.cx = 0.0f;
             g_camera.cz = 0.0f;
         }
+        if (key == GLFW_KEY_SPACE) g_game.startNextWave();
 
         if (key == GLFW_KEY_1) g_game.selectTowerType(0);
         if (key == GLFW_KEY_2) g_game.selectTowerType(1);
         if (key == GLFW_KEY_3) g_game.selectTowerType(2);
+
+        if (key == GLFW_KEY_1) g_game.toggleBuildMode(0);
+        if (key == GLFW_KEY_2) g_game.toggleBuildMode(1);
+        if (key == GLFW_KEY_3) g_game.toggleBuildMode(2);
     }
 }
 
-int main(void) {
+int main(int argc, char** argv) {
     if (!glfwInit()) return -1;
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Tower Defense 3D", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        return -1;
-    }
+    int glut_argc = 1;
+    char* glut_argv[] = { (char*)"TowerDefense" };
+    glutInit(&glut_argc, glut_argv);
+
+    GLFWmonitor* primary_monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(primary_monitor);
+
+    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "Tower Defense 3D", NULL, NULL);
+    if (!window) { glfwTerminate(); return -1; }
+
+    glfwSetWindowPos(window, 0, 0);
 
     glfwMakeContextCurrent(window);
+    glfwGetCursorPos(window, &g_last_mouse_x, &g_last_mouse_y);
+
+    glViewport(0, 0, mode->width, mode->height);
+
     if (glewInit() != GLEW_OK) return -1;
 
-    int w, h;
-    glfwGetFramebufferSize(window, &w, &h);
-    framebuffer_size_callback(window, w, h);
+    int screen_width, screen_height;
+    glfwGetFramebufferSize(window, &screen_width, &screen_height);
+    framebuffer_size_callback(window, screen_width, screen_height);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetMouseButtonCallback(window, mouse_button_callback);
@@ -113,67 +142,49 @@ int main(void) {
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
     glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
     glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
 
     g_game.init();
-    g_lastTime = (float)glfwGetTime();
+    g_previous_time = (float)glfwGetTime();
 
     while (!glfwWindowShouldClose(window)) {
-        float current_frame_time = (float)glfwGetTime();
-        float delta_step = current_frame_time - g_lastTime;
-        g_lastTime = current_frame_time;
+        float current_time_stamp = (float)glfwGetTime();
+        float frame_delta = current_time_stamp - g_previous_time;
+        g_previous_time = current_time_stamp;
 
-        double mouse_x, mouse_y;
-        int screen_w, screen_h;
-        glfwGetCursorPos(window, &mouse_x, &mouse_y);
-        glfwGetWindowSize(window, &screen_w, &screen_h);
+        if (frame_delta > 0.05f) frame_delta = 0.05f;
 
-        const int margin = 15;
+        double cursor_x, cursor_y;
+        int win_w, win_h;
+        glfwGetCursorPos(window, &cursor_x, &cursor_y);
+        glfwGetWindowSize(window, &win_w, &win_h);
 
-        float rad = g_camera.angleY * 3.14159f / 180.0f;
-        float fwdX = sinf(rad);
-        float fwdZ = -cosf(rad);
-        float rgtX = cosf(rad);
-        float rgtZ = sinf(rad);
+        float yaw_rad = g_camera.angleY * 3.14159f / 180.0f;
+        float dir_fwd_x = sinf(yaw_rad);
+        float dir_fwd_z = -cosf(yaw_rad);
+        float dir_rgt_x = cosf(yaw_rad);
+        float dir_rgt_z = sinf(yaw_rad);
 
-        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-            g_camera.cx += fwdX * MOVE_SPEED * delta_step;
-            g_camera.cz += fwdZ * MOVE_SPEED * delta_step;
-        }
-        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-            g_camera.cx -= fwdX * MOVE_SPEED * delta_step;
-            g_camera.cz -= fwdZ * MOVE_SPEED * delta_step;
-        }
-        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-            g_camera.cx -= rgtX * MOVE_SPEED * delta_step;
-            g_camera.cz -= rgtZ * MOVE_SPEED * delta_step;
-        }
-        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-            g_camera.cx += rgtX * MOVE_SPEED * delta_step;
-            g_camera.cz += rgtZ * MOVE_SPEED * delta_step;
-        }
+        const int screen_margin = 15;
+        if (cursor_x < screen_margin) { g_camera.cx -= dir_rgt_x * CAMERA_MOVE_SPEED * frame_delta; g_camera.cz -= dir_rgt_z * CAMERA_MOVE_SPEED * frame_delta; }
+        if (cursor_x > win_w - screen_margin) { g_camera.cx += dir_rgt_x * CAMERA_MOVE_SPEED * frame_delta; g_camera.cz += dir_rgt_z * CAMERA_MOVE_SPEED * frame_delta; }
+        if (cursor_y < screen_margin) { g_camera.cx += dir_fwd_x * CAMERA_MOVE_SPEED * frame_delta; g_camera.cz += dir_fwd_z * CAMERA_MOVE_SPEED * frame_delta; }
+        if (cursor_y > win_h - screen_margin) { g_camera.cx -= dir_fwd_x * CAMERA_MOVE_SPEED * frame_delta; g_camera.cz -= dir_fwd_z * CAMERA_MOVE_SPEED * frame_delta; }
 
-        if (mouse_x < margin) { g_camera.cx -= rgtX * MOVE_SPEED * delta_step; g_camera.cz -= rgtZ * MOVE_SPEED * delta_step; }
-        if (mouse_x > screen_w - margin) { g_camera.cx += rgtX * MOVE_SPEED * delta_step; g_camera.cz += rgtZ * MOVE_SPEED * delta_step; }
-        if (mouse_y < margin) { g_camera.cx += fwdX * MOVE_SPEED * delta_step; g_camera.cz += fwdZ * MOVE_SPEED * delta_step; }
-        if (mouse_y > screen_h - margin) { g_camera.cx -= fwdX * MOVE_SPEED * delta_step; g_camera.cz -= fwdZ * MOVE_SPEED * delta_step; }
+        if (g_camera.cx < -MAP_BOUNDARY) g_camera.cx = -MAP_BOUNDARY;
+        if (g_camera.cx > MAP_BOUNDARY) g_camera.cx = MAP_BOUNDARY;
+        if (g_camera.cz < -MAP_BOUNDARY) g_camera.cz = -MAP_BOUNDARY;
+        if (g_camera.cz > MAP_BOUNDARY) g_camera.cz = MAP_BOUNDARY;
 
-        if (g_camera.cx < -MAP_LIMIT) g_camera.cx = -MAP_LIMIT;
-        if (g_camera.cx > MAP_LIMIT) g_camera.cx = MAP_LIMIT;
-        if (g_camera.cz < -MAP_LIMIT) g_camera.cz = -MAP_LIMIT;
-        if (g_camera.cz > MAP_LIMIT) g_camera.cz = MAP_LIMIT;
-
-        if (delta_step > 0.05f) delta_step = 0.05f;
-
-        if (!g_game.gameOver) g_game.update(delta_step);
+        if (!g_game.gameOver) g_game.update(frame_delta);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
 
-        float light_pos[] = { 0.0f, 50.0f, 20.0f, 1.0f };
-        glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+        float light_source_pos[] = { 0.0f, 50.0f, 20.0f, 1.0f };
+        glLightfv(GL_LIGHT0, GL_POSITION, light_source_pos);
 
         g_camera.apply();
         g_game.render();
